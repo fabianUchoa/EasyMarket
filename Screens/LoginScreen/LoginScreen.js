1aaf81b20e5756073da9e85e79ba1e6c612683b2
Fabian Uchoa <fabian@forlogic.net>
2025-01-13 11:57:54 -0300

    homeScreen

---
+++ Screens/LoginScreen/LoginScreen.js
@@ -0,0 +1,28 @@
+function changeInformationContentToSafe(){
+    var safeOption = document.getElementById("safeOption");
+    var img = document.getElementById("imgChanger");
+    var text = document.getElementById("textChanger");
+    
+    
+    img.src ="/Imgs/LoginScreen/imgSafe.svg"
+    text.innerHTML = "Security in sales is essential to protect both the seller and the buyer during transactions. It is important to use reliable platforms and avoid sharing unnecessary personal or financial information. Verifying the authenticity of the buyer or seller and using secure payment methods, such as payment intermediaries, reduces the risk of fraud. Additionally, documents like receipts and contracts are crucial to ensure legal support. With secure practices, it is possible to create a trustworthy environment that benefits both parties."
+
+}
+
+function changeInformationContentToLowTaxes(){
+    var lowTaxesOption = document.getElementById("lowTaxesOption");
+    var img = document.getElementById("imgChanger");
+    var text = document.getElementById("textChanger");
+    
+    img.src ="/Imgs/LoginScreen/imgLowTaxes.png"
+    text.innerHTML = "Low fees are a competitive advantage that attracts consumers and boosts business. They make products and services more accessible, especially for small entrepreneurs and customers with limited budgets. Additionally, reduced fees can increase transaction volumes, driving sustainable growth. Companies offering favorable conditions gain the trust and loyalty of their audience. Thus, low fees become a strategic factor for expanding opportunities and promoting economic development."
+}
+
+function changeInformationContentToEase(){
+    var easeOption = document.getElementById("easeOption");
+    var img = document.getElementById("imgChanger");
+    var text = document.getElementById("textChanger");
+
+    img.src ="/Imgs/LoginScreen/imgEase.svg"
+    text.innerHTML ="Selling has never been easier! With our platform, you can streamline your sales process and focus on what truly matters—your business growth. We provide intuitive tools, secure payment options, and seamless communication features to ensure every transaction is hassle-free. Whether you're a seasoned entrepreneur or just starting, our system is designed to save you time and effort while maximizing your success. Experience the convenience of selling with us today and watch your business thrive!"
+}
